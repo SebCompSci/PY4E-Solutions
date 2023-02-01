2d1baad2fea6cb84d11797e9ecce53771bb49834
SebCompSci <sebcompsci@gmail.com>
2023-02-01 11:52:13 +0100

    Added solutions for chapter 13

---
+++ CH13/EX13_01.py
@@ -0,0 +1,26 @@
+#Write a program that prompts for a URL, reads the XML data from that URL using urllib and then parses and extracts the comment counts from the XML data and computes the sum of the numbers.
+#We provide 2 files for this assignment.
+#One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+# sample data: 'http://py4e-data.dr-chuck.net/comments_42.xml' sum = 2553.
+# acutal data: 'http://py4e-data.dr-chuck.net/comments_1730544.xml' sum ends with 71.
+#The data consists of a number of names and comment counts in XML as follows:
+# <comment>
+#   <name>Matthias</name>
+#   <count>97</count>
+# </comment>
+#To make the code a little simpeler, you can use an XPath selector string to look through the entire tree of XML for any tag named 'count':
+# count_loc = data.findall('.//count')
+
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input("Enter URL: ")
+fhand = urllib.request.urlopen(url).read()
+data = ET.fromstring(fhand)
+count_loc = data.findall('.//count')
+
+num_sum = 0
+for count in count_loc:
+    num_sum += int(count.text)
+
+print(num_sum)
